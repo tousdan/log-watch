b1f2963e1c15d7e8ba63a591c9caf17964941b27
tousdan <tousdan@gmail.com>
2015-02-23 22:38:51 -0500

    moved requests to their own file. moved logging config to a file.

---
+++ api_requests.py
@@ -0,0 +1,169 @@
+import time, logging
+import requests
+import json
+
+from requests.auth import HTTPBasicAuth
+
+#Setup logging
+logger = logging.getLogger(__name__)
+
+def totimestamp(dt):
+  """Converts datetime to timestamp"""
+
+  return int(time.mktime(dt.timetuple())) * 1000 
+
+def shards_failed(response):
+  return response['_shards']['failed'] > 0
+
+
+
+class RequestUnsuccesfulError(Exception):
+  def __init__(self, status_code):
+    self.status_code = status_code
+
+  def __str__(self):
+    return repr(self.status_code)
+
+class Request(object):
+  """
+  Base request to the elastic search instance search API
+  """
+  def __init__(self, indexes, request_data, config, logger=None):
+    self.indexes = indexes
+    self.request_data = request_data
+    self.config = config
+    self.environment = self.config_value('environment')
+    self.logger = logger or logging.getLogger(__name__)
+
+
+  def run(self):
+    response = requests.post("%s%s/_search" % (self.config['es_url'], ",".join(self.indexes)), 
+                            auth=HTTPBasicAuth(self.config['username'], self.config['password']), 
+                            data=json.dumps(self.request_data))
+    
+    self.logger.debug("Launching request")
+    if response.status_code == 200:
+      json_response = response.json()
+      if shards_failed(json_response):
+        self.logger.error("Some shards failed to respond")
+
+      page_result = json_response['hits']
+      
+      return self.parse(page_result['hits'])
+    else:
+      raise RequestUnsuccesfulError(response.status_code)
+
+  def read_field(self, entry, fieldname):
+    if fieldname in entry['fields']:
+      return entry['fields'][fieldname][0]
+
+    return None
+
+  def read_fields(self, entry, fieldnames):
+    return tuple([self.read_field(entry, field) for field in fieldnames])
+
+  def parse(self, hits):
+    return hits
+
+  def config_value(self, key):
+    if key in self.config:
+      return self.config[key]
+
+    return None
+
+class ErrorTransactionListing(Request):
+  """
+  Fetches the API transaction which ended in error
+  """
+  def __init__(self, indexes, timerange, config):
+    request_data = {
+      "size": 1000,
+      "fields": ["transactionId"],
+      "query": {
+        "filtered": {
+          "filter": {
+            "bool": {
+              "must": [
+                { "term":  { "level": "error" } },
+                { "range": { "@timestamp" : { "from": totimestamp(timerange[0]), "to": totimestamp(timerange[1]) } } }
+              ]
+            }
+          }
+        }
+      }
+    }
+
+    Request.__init__(self, indexes, request_data, config)
+
+  def parse(self, hits):
+    self.logger.debug("Received %s results", len(hits))
+    return [tx['fields']['transactionId'][0] for tx in hits]
+
+class TransactionDetail(Request):
+  """
+  Fetches the detail of an API transaction
+  """
+  def __init__(self, indexes, transactionId, config):
+    self.transactionId = transactionId
+
+    request_data = {
+       "sort": "@timestamp",
+       "fields": ["@timestamp", "loggerName", "message", "stacktrace", "transactionId", "proteus-username", "referer"],
+       "query": {
+        "filtered": {
+          "query": {
+            "bool": {
+              "should": [
+                {"query_string": { "query": "transactionId:\"%s\"" % (transactionId, ) } }
+              ]
+            }
+          }
+        }
+      }
+    }
+
+    Request.__init__(self, indexes, request_data, config)
+
+  def log_to_slack(self, message):
+    slack_conf = self.config_value('slack_bot')
+
+    requests.get("https://slack.com/api/chat.postMessage?token=%s&channel=%s&text=%s" % (slack_conf['token'], slack_conf['channel'], message))
+
+  def parse(self, hits):
+    result = []
+    self.logger.debug("Fetching transaction detail for %s", self.transactionId)
+    username = None
+    referer = None
+    for entry in hits:
+      tstamp, tx, className, msg, stack = self.read_fields(entry, ('@timestamp', 'transactionId', 'loggerName', 'message', 'stacktrace'))
+      raw_user, raw_referer = self.read_fields(entry, ('proteus-username', 'referer'))
+
+      result.append({
+        'transaction': tx,
+        'time': tstamp,
+        'class':  className,
+        'message': msg,
+        'stack': stack,
+      })
+
+      if raw_user:
+        username = raw_user
+
+      if raw_referer:
+        referer = raw_referer
+
+    for parsed in result:
+      parsed['username'] = username
+      parsed['referer'] = referer
+
+    error_message = result[-1]['message'][:50]
+    message = "[%s] %s -> %s (%s)" % (self.environment, self.transactionId, error_message, username)
+
+    if self.config_value('slack_bot'):
+      self.log_to_slack(message)
+      
+
+    self.logger.info(message)
+
+    return result
+
--- main.py
+++ main.py
@@ -1,28 +1,35 @@
-import requests, json
-import pprint, time
-import sys, os
-import logging
-import doctest
-import argparse
+import json, time, sys, os
+import logging, argparse
+import logging.config
+
+from requests.exceptions import Timeout, ConnectionError
 
 from datetime import datetime, timedelta
 
 from requests.auth import HTTPBasicAuth
 
-#Setup logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+from api_requests import TransactionDetail, ErrorTransactionListing, RequestUnsuccesfulError
 
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
+logging.config.fileConfig('logging.conf')
+#Setup logging
+logging.basicConfig(level=logging.INFO)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s %(message)s',  datefmt='%I:%M:%S %p')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
-pp = pprint.PrettyPrinter(indent=2)
+ch.setFormatter(formatter)
+
+logger = logging.getLogger(__name__)
+
+
+def indexes_for_date(environment, dates):
+  return set(["cc-proteus-%s.%s" % (environment, str(dt.isocalendar()[1]), ) for dt in dates])
+
 
 def run(config):
+
   environment = config['environment']
 
   try:
@@ -30,7 +37,7 @@ def run(config):
   except KeyError: 
     sleep_time = 60
 
-  tx_dir = "C:/temp/transactions/%s" % (environment, )
+  tx_dir = os.path.join(config['logs_dir'], environment)
 
   if not os.path.exists(tx_dir):
     os.makedirs(tx_dir)
@@ -53,9 +60,9 @@ def run(config):
           detail = TransactionDetail(indexes, tx, config).run()
 
           save_transaction(tx_dir, date_folder, tx, detail)
-    except requests.exceptions.Timeout, e:
+    except Timeout, e:
       logger.warn("Timeout occured - retrying in a bit: %s", e)
-    except requests.exceptions.ConnectionError, e:
+    except ConnectionError, e:
       logger.warn("Connection error occured - retrying in a bit: %s", e)
     except RequestUnsuccesfulError, e:
       logger.warn("Response returned unhandled status code: %s", e)
@@ -78,187 +85,16 @@ def save_transaction(tx_dir, date_folder, transactionId, detail):
   with open(path, "w") as output:
     output.write(json.dumps(detail, sort_keys=True, indent=4, separators=(',', ': ')))
 
-def shards_failed(response):
-  return response['_shards']['failed'] > 0
-
-def indexes_for_date(environment, dates):
-  return set(["cc-proteus-%s.%s" % (environment, str(dt.isocalendar()[1]), ) for dt in dates])
-
-def totimestamp(dt):
-  """Converts datetime to timestamp
-
-  >>> totimestamp(datetime(2015, 02, 11))
-  1423630800000L
-
-  >>> totimestamp(datetime(2015, 02, 11, 23, 59, 59))
-  1423717199000L
-  """
-
-  return int(time.mktime(dt.timetuple())) * 1000 
-
-class Request(object):
-
-  def __init__(self, indexes, request_data, config):
-    self.indexes = indexes
-    self.request_data = request_data
-    self.config = config
-    self.environment = self.config_value('environment')
-
-
-  def run(self):
-    response = requests.post("%s%s/_search" % (config['es_url'], ",".join(self.indexes)), 
-                            auth=HTTPBasicAuth(config['username'], config['password']), 
-                            data=json.dumps(self.request_data))
-    
-    logger.debug("Launching request")
-    if response.status_code == 200:
-      json_response = response.json()
-      if shards_failed(json_response):
-        logger.error("Some shards failed to respond")
-
-      page_result = json_response['hits']
-      
-      return self.parse(page_result['hits'])
-    else:
-      raise RequestUnsuccesfulError(response.status_code)
-
-  def read_field(self, entry, fieldname):
-    if fieldname in entry['fields']:
-      return entry['fields'][fieldname][0]
-
-    return None
-
-  def read_fields(self, entry, fieldnames):
-    return tuple([self.read_field(entry, field) for field in fieldnames])
-
-  def parse(self, hits):
-    return hits
-
-  def config_value(self, key):
-    if key in self.config:
-      return self.config[key]
-
-    return None
-
-
-class TransactionDetail(Request):
-  def __init__(self, indexes, transactionId, config):
-    self.transactionId = transactionId
-
-    request_data = {
-       "sort": "@timestamp",
-       "fields": ["@timestamp", "loggerName", "message", "stacktrace", "transactionId", "proteus-username", "referer"],
-       "query": {
-        "filtered": {
-          "query": {
-            "bool": {
-              "should": [
-                {"query_string": { "query": "transactionId:\"%s\"" % (transactionId, ) } }
-              ]
-            }
-          }
-        }
-      }
-    }
-
-    Request.__init__(self, indexes, request_data, config)
-
-  def log_to_slack(self, message):
-    slack_conf = self.config_value('slack_bot')
-
-    requests.get("https://slack.com/api/chat.postMessage?token=%s&channel=%s&text=%s" % (slack_conf['token'], slack_conf['channel'], message))
-
-  def parse(self, hits):
-    result = []
-    logger.debug("Fetching transaction detail for %s", self.transactionId)
-    username = None
-    referer = None
-    for entry in hits:
-      tstamp, tx, className, msg, stack = self.read_fields(entry, ('@timestamp', 'transactionId', 'loggerName', 'message', 'stacktrace'))
-      raw_user, raw_referer = self.read_fields(entry, ('proteus-username', 'referer'))
-
-      result.append({
-        'transaction': tx,
-        'time': tstamp,
-        'class':  className,
-        'message': msg,
-        'stack': stack,
-      })
-
-      if raw_user:
-        username = raw_user
-
-      if raw_referer:
-        referer = raw_referer
-
-    for parsed in result:
-      parsed['username'] = username
-      parsed['referer'] = referer
-
-    error_message = result[-1]['message'][:50]
-    message = "[%s] %s -> %s (%s)" % (self.environment, self.transactionId, error_message, username)
-
-    if self.config_value('slack_bot'):
-      self.log_to_slack(message)
-      
-
-    logger.info(message)
-
-    return result
-
-
-class ErrorTransactionListing(Request):
-  def __init__(self, indexes, timerange, config):
-    request_data = {
-      "size": 1000,
-      "fields": ["transactionId"],
-      "query": {
-        "filtered": {
-          "filter": {
-            "bool": {
-              "must": [
-                { "term":  { "level": "error" } },
-                { "range": { "@timestamp" : { "from": totimestamp(timerange[0]), "to": totimestamp(timerange[1]) } } }
-              ]
-            }
-          }
-        }
-      }
-    }
-
-    Request.__init__(self, indexes, request_data, config)
-
-  def parse(self, hits):
-    logger.debug("Received %s results", len(hits))
-    return [tx['fields']['transactionId'][0] for tx in hits]
-
-class RequestUnsuccesfulError(Exception):
-  def __init__(self, status_code):
-    self.status_code = status_code
-
-  def __str__(self):
-    return repr(self.status_code)
-
-
-def validate_program():
-  import doctest
-  failures, ran = doctest.testmod()
-
-  if failures > 0:
-    sys.exit(-1)
-
 def read_config(config_file):
   config = json.loads(config_file.read())
 
-  for key in ['username', 'password', 'environment', 'es_url']:
+  for key in ['username', 'password', 'environment', 'es_url', 'logs_dir']:
     if not key in config:
       raise NameError(key)
   
   return config
 
 if __name__ == '__main__':
-  validate_program()
-
   parser = argparse.ArgumentParser(description="Proteus API error transaction saver (and broadcaster!)")
   parser.add_argument("--config", action='store', dest='config', help='location of configuration file to use (defaults to config.json)', default='config.json', type=open)
   args = parser.parse_args()
